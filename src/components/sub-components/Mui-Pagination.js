a46508c98a39d1899d703db7ce70a5517ec5e6ed
Emmanuel Alcoriza <alcorizaem18@gmail.com>
2024-04-04 11:36:27 +0800

    adjusted the color of pagination

--- src/components/sub-components/Mui-Pagination.js
+++ src/components/sub-components/Mui-Pagination.js
@@ -1,25 +1,36 @@
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const BasicPagination = ({ page, setCurrentPage, totalPageCount }) => {
   const handleChange = (event, value) => {
     setCurrentPage(value);
   };
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#20b2aa ",
+      },
+    },
+  });
+
   return (
-    <div className="pagination">
-      <Stack spacing={2}>
-        <Pagination
-          count={totalPageCount}
-          onChange={handleChange}
-          defaultPage={1}
-          page={page}
-          color="primary"
-          shape="rounded"
-          variant="outlined"
-        />
-      </Stack>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="pagination">
+        <Stack spacing={2}>
+          <Pagination
+            count={totalPageCount}
+            onChange={handleChange}
+            defaultPage={1}
+            page={page}
+            color="primary"
+            shape="rounded"
+            variant="outlined"
+          />
+        </Stack>
+      </div>
+    </ThemeProvider>
   );
 };
 
