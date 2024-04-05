553a4c16ec2d009fc842264894e8d3479a83c651
Emmanuel Alcoriza <alcorizaem18@gmail.com>
2024-04-05 22:22:25 +0800

    deleted unecessary component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
-import NewPagination from "./components/sub-components/New_Pagination";
-
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContent />
       <Footer />
-      {/* <NewPagination /> */}
     </div>
   );
 }
