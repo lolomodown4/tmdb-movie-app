ce8605e598ec358d70cfb15fe83f9b29b14f6c9b
Emmanuel Alcoriza <alcorizaem18@gmail.com>
2024-04-06 13:04:13 +0800

    burder done but found some bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [isBurgerClicked, setIsBurgerClicked] = useState(false);
 
+  const [isTrendingClicked, setIsTrendingClicked] = useState(true);
+  const [isMoviesClicked, setIsMoviesClicked] = useState(false);
+  const [isTv_SeriesClicked, setIsTv_SeriesClicked] = useState(false);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+
   const windowSize = useRef(window.innerWidth);
 
   const handleClick = () => {
@@ -14,17 +19,45 @@ const Header = () => {
 
   const handleTrending = () => {
     setIsBurgerClicked(false);
+
+    setIsTrendingClicked(true);
+
+    /* false other states categories */
+    setIsMoviesClicked(false);
+    setIsTv_SeriesClicked(false);
+    setIsSearchClicked(false);
   };
   const handleMovies = () => {
     setIsBurgerClicked(false);
+
+    setIsMoviesClicked(true);
+
+    /* false other states categories */
+    setIsTrendingClicked(false);
+    setIsTv_SeriesClicked(false);
+    setIsSearchClicked(false);
   };
 
   const handleTv_series = () => {
     setIsBurgerClicked(false);
+
+    setIsTv_SeriesClicked(true);
+
+    /* false other states categories */
+    setIsTrendingClicked(false);
+    setIsMoviesClicked(false);
+    setIsSearchClicked(false);
   };
 
   const handleSearch = () => {
     setIsBurgerClicked(false);
+
+    setIsSearchClicked(true);
+
+    /* false other states categories */
+    setIsTrendingClicked(false);
+    setIsMoviesClicked(false);
+    setIsTv_SeriesClicked(false);
   };
 
   return (
@@ -52,12 +85,20 @@ const Header = () => {
           <nav className="burger-nav">
             <ul className="burger-links">
               <li>
-                <Link to="/" onClick={handleTrending} className="links">
+                <Link
+                  to="/"
+                  onClick={handleTrending}
+                  className={isTrendingClicked ? "selected-link" : "links"}
+                >
                   Trending
                 </Link>
               </li>
               <li>
-                <Link to="/movies" onClick={handleMovies} className="links">
+                <Link
+                  to="/movies"
+                  onClick={handleMovies}
+                  className={isMoviesClicked ? "selected-link" : "links"}
+                >
                   Movies
                 </Link>
               </li>
@@ -65,13 +106,17 @@ const Header = () => {
                 <Link
                   to="/tv-series"
                   onClick={handleTv_series}
-                  className="links"
+                  className={isTv_SeriesClicked ? "selected-link" : "links"}
                 >
                   Tv-Series
                 </Link>
               </li>
               <li>
-                <Link to="/search" onClick={handleSearch} className="links">
+                <Link
+                  to="/search"
+                  onClick={handleSearch}
+                  className={isSearchClicked ? "selected-link" : "links"}
+                >
                   Search
                 </Link>
               </li>
